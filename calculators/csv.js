6b0ef25ae93c513b8699327190af6dc05e1f2b72
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2017-03-12 17:57:50 +0500

    added theraggy calulator

---
+++ calculators/csv.js
@@ -0,0 +1,142 @@
+var rates = [
+ {
+   "Collection": "Jita",
+   "Destination": "1DQ1-A",
+   "Rate": 720
+ },
+ {
+   "Collection": "Jita",
+   "Destination": "7G-QIG",
+   "Rate": 765
+ },
+ {
+   "Collection": "Jita",
+   "Destination": "8RQJ-2",
+   "Rate": 765
+ },
+ {
+   "Collection": "Jita",
+   "Destination": "D-W7F0",
+   "Rate": 720
+ },
+ {
+   "Collection": "Jita",
+   "Destination": "R5-MM8",
+   "Rate": 765
+ },
+ {
+   "Collection": "Amarr",
+   "Destination": "1DQ1-A",
+   "Rate": 523
+ },
+ {
+   "Collection": "Amarr",
+   "Destination": "7G-QIG",
+   "Rate": 540
+ },
+ {
+   "Collection": "Amarr",
+   "Destination": "8RQJ-2",
+   "Rate": 540
+ },
+ {
+   "Collection": "Amarr",
+   "Destination": "D-W7F0",
+   "Rate": 523
+ },
+ {
+   "Collection": "Amarr",
+   "Destination": "R5-MM8",
+   "Rate": 540
+ },
+ {
+   "Collection": "1DQ1-A",
+   "Destination": "Jita",
+   "Rate": 720
+ },
+ {
+   "Collection": "7G-QIG",
+   "Destination": "Jita",
+   "Rate": 765
+ },
+ {
+   "Collection": "8RQJ-2",
+   "Destination": "Jita",
+   "Rate": 765
+ },
+ {
+   "Collection": "D-W7F0",
+   "Destination": "Jita",
+   "Rate": 720
+ },
+ {
+   "Collection": "R5-MM8",
+   "Destination": "Jita",
+   "Rate": 765
+ },
+ {
+   "Collection": "1DQ1-A",
+   "Destination": "Amarr",
+   "Rate": 495
+ },
+ {
+   "Collection": "7G-QIG",
+   "Destination": "Amarr",
+   "Rate": 540
+ },
+ {
+   "Collection": "8RQJ-2",
+   "Destination": "Amarr",
+   "Rate": 540
+ },
+ {
+   "Collection": "D-W7F0",
+   "Destination": "Amarr",
+   "Rate": 495
+ },
+ {
+   "Collection": "R5-MM8",
+   "Destination": "Amarr",
+   "Rate": 540
+ },
+ {
+   "Collection": "7G-QIG",
+   "Destination": "1DQ1-A",
+   "Rate": 90
+ },
+ {
+   "Collection": "8RQJ-2",
+   "Destination": "1DQ1-A",
+   "Rate": 90
+ },
+ {
+   "Collection": "D-W7F0",
+   "Destination": "1DQ1-A",
+   "Rate": 90
+ },
+ {
+   "Collection": "R5-MM8",
+   "Destination": "1DQ1-A",
+   "Rate": 90
+ },
+ {
+   "Collection": "1DQ1-A",
+   "Destination": "7G-QIG",
+   "Rate": 90
+ },
+ {
+   "Collection": "1DQ1-A",
+   "Destination": "8RQJ-2",
+   "Rate": 90
+ },
+ {
+   "Collection": "1DQ1-A",
+   "Destination": "D-W7F0",
+   "Rate": 90
+ },
+ {
+   "Collection": "1DQ1-A",
+   "Destination": "R5-MM8",
+   "Rate": 90
+ }
+]
