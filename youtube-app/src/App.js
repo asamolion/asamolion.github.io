733e7a79482f984656b920e752377138bf3a4c34
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-07-27 11:45:49 +0500

    added app;

---
+++ youtube-app/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import "./App.css";
+import SearchForm from "./SearchForm.js";
+import Result from "./Result.js";
+import VideoPlayer from "./VideoPlayer.js";
+import * as youtube from "./youtubeapi.js";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.searchYoutube = this.searchYoutube.bind(this);
+    this.playVideo = this.playVideo.bind(this);
+    this.state = {
+      playVid: false,
+      vidId: "",
+      results: []
+    };
+  }
+
+  playVideo(vidId) {
+    this.setState({
+      playVid: true,
+      vidId
+    });
+  }
+
+  searchYoutube(query) {
+    youtube.search(jsonData => {
+      this.setState({
+        results: jsonData.items
+      });
+    }, query);
+  }
+
+  componentDidMount() {
+    this.searchYoutube("");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <SearchForm searchHandler={this.searchYoutube} />
+        </div>
+
+        {this.state.playVid &&
+          <div className="player-container">
+            <VideoPlayer vidId={this.state.vidId} />
+          </div>}
+
+        <div className="result-container">
+          {this.state.results.map(item =>
+            <Result
+              key={item.etag}
+              imgurl={item.snippet.thumbnails.medium.url}
+              title={item.snippet.title}
+              description={item.snippet.description}
+              vidId={item.id.videoId}
+              play={this.playVideo}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ youtube-app/src/SearchForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./SearchForm.css";
+
+class SearchForm extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      query: ""
+    };
+  }
+
+  static propTypes = {
+    searchHandler: PropTypes.func
+  };
+
+  handleChange(query) {
+    this.setState({
+      query
+    });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.props.searchHandler(this.state.query);
+  }
+
+  render() {
+    return (
+      <div className="search-container">
+        <form action="" className="search-form">
+          <input
+            onChange={evt => this.handleChange(evt.target.value)}
+            className="search-input"
+            type="text"
+            placeholder="Search"
+          />
+          <button
+            onClick={this.handleSubmit}
+            className="search-button"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchForm;
